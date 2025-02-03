cc382f0d4964b26bd6bde980a471f632289cbc15
Leonardo Lazilha <leonardolazilha@MacBook-Pro-de-Leonardo.local>
2025-02-03 15:40:38 -0300

    usando clausula distinct

---
+++ mudando-apresentacao-dos-dados/distinct_select.sql
@@ -0,0 +1,12 @@
+/* 
+    distinct
+
+    remove duplicatas em uma consulta, retorna apenas valores unicos
+    
+*/
+
+SELECT * FROM TABELA_DE_PRODUTOS;
+
+SELECT DISTINCT EMBALAGEM FROM TABELA_DE_PRODUTOS
+
+
