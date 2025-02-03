3d65cb64b7e98beb14df82c396b4c116a90c88fd
Leonardo Lazilha <leonardolazilha@MacBook-Pro-de-Leonardo.local>
2025-02-03 15:42:55 -0300

    desafio da aula

--- mudando-apresentacao-dos-dados/distinct_select.sql
+++ mudando-apresentacao-dos-dados/distinct_select.sql
@@ -8,5 +8,23 @@
 SELECT * FROM TABELA_DE_PRODUTOS;
 
 SELECT DISTINCT EMBALAGEM FROM TABELA_DE_PRODUTOS
+WHERE SABOR = 'Maca';
+
+SELECT DISTINCT EMBALAGEM, SABOR FROM TABELA_DE_PRODUTOS
+
+
+
+/* 
+    Desafio:
+    Tendo em conta nossa tabela de produtos, execute uma consulta para obter o número de sabores existentes. 
+    Especifique o SQL que você executou para obter o resultado e a resposta propriamente dita.  
+*/
+
+SELECT * FROM TABELA_DE_PRODUTOS;
+
+SELECT DISTINCT SABOR FROM TABELA_DE_PRODUTOS;
+
+
+
 
 
